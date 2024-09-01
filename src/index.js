db3301a7983be760f13f5b34dde57fa793fe15a0
mdasif6565 <mdasifdev123@gmail.com>
2024-09-01 17:18:18 +0600

    full responsive complete

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ mobile_icon.addEventListener("click",function(){
     mobile_menu.classList.toggle("hidden");
 });
 
+
+// selecting all a element on the page
+const links = document.querySelectorAll('a');
+
+// adding a click event on all elements 
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // if we click first thing is deleting the active class from all link
+    links.forEach((link) => {
+      link.classList.remove('active')
+    })
+    // then in the end add the active class only in the correct one
+    e.target.classList.add('active')
+  })
+})
